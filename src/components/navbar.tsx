b840070a455dc2a0e8ff4e9e263efe6011c3f3d0
jrdeavila <jrdeavila@unicesar.edu.co>
2024-07-21 19:04:33 -0400

    Refactor navbar component and remove unused code

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,17 +2,10 @@ import { Link } from "@nextui-org/link";
 import {
   NavbarBrand,
   NavbarContent,
-  NavbarItem,
-  NavbarMenu,
-  NavbarMenuItem,
-  NavbarMenuToggle,
   Navbar as NextUINavbar,
 } from "@nextui-org/navbar";
-import { link as linkStyles } from "@nextui-org/theme";
-import clsx from "clsx";
 
 import { Logo } from "@/components/icons";
-import { siteConfig } from "@/config/site";
 
 export const Navbar = () => {
   return (
@@ -34,48 +27,7 @@ export const Navbar = () => {
             <Logo />
           </Link>
         </NavbarBrand>
-        <div className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <Link
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-mediumi text-white font-bold"
-                )}
-                href={item.href}
-              >
-                {item.label}
-              </Link>
-            </NavbarItem>
-          ))}
-        </div>
       </NavbarContent>
-
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <NavbarMenuToggle className="text-white" />
-      </NavbarContent>
-
-      <NavbarMenu>
-        <div className="mx-4 mt-4 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                    ? "danger"
-                    : "foreground"
-                }
-                href="#"
-                size="lg"
-              >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </div>
-      </NavbarMenu>
     </NextUINavbar>
   );
 };
