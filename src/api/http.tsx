26a85301ed6cb5464af79dfb9a627ed9331053b2
jrdeavila <jrdeavila@unicesar.edu.co>
2024-07-31 16:35:31 -0400

    feat: Add Bootstrap v5.3.3 for styling consistency

---
+++ src/api/http.tsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+const apiUrl = "https://digiturnov2.ccvalledupar.org.co/api";
+
+const httpClient = axios.create({
+  baseURL: apiUrl,
+});
+
+httpClient.interceptors.request.use(function (config) {
+  config.headers["Content-Type"] = "application/json";
+  return config;
+});
+
+const showError = (statusCode: number) => {
+  const messages: {
+    [key: number]: string;
+  } = {
+    401: "No autorizado",
+    403: "Prohibido",
+    404: "No encontrado",
+    500: "Error interno del servidor",
+    422: "Error de validación",
+    405: "Acción no permitida",
+    0: "No se puede conectar al servidor",
+  };
+  toast.error(messages[statusCode] || "Ha ocurrido un error inesperado", {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+httpClient.interceptors.response.use(
+  function (config) {
+    return config;
+  },
+  (error) => {
+    if (error.response) {
+      showError(error.response.status);
+    } else {
+      showError(0);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export async function get<T>(endpoint: string): Promise<T> {
+  const response = await httpClient.get<T>(endpoint);
+  return response.data;
+}
+
+export async function post<T>(endpoint: string, data: object): Promise<T> {
+  const response = await httpClient.post<T>(endpoint, data);
+  return response.data;
+}
+
+export async function patch<T>(endpoint: string, data: object): Promise<T> {
+  const response = await httpClient.patch(endpoint, data);
+  return response.data;
+}
+
+export async function borrar<T>(endpoint: string): Promise<T> {
+  const response = await httpClient.delete(endpoint);
+  return response.data;
+}
---
+++ src/components/BodyReception.tsx
@@ -0,0 +1,388 @@
+import { useEffect, useState } from "react";
+import { Cliente, Modulo, Perfil, Turno } from "../models/interfaces";
+import { get, post, patch, borrar } from "@/api/http";
+import useMyModule from "@/hooks/use-my-module";
+
+export default function BodyReception() {
+  const [clientes, setClientes] = useState<Cliente[]>([]);
+  const [perfiles, setPerfiles] = useState<Perfil[]>([]);
+  const [cedula, setCedula] = useState<string>("");
+  const [nombre, setNombre] = useState<string>("");
+  const [tipoCliente, setTipoCliente] = useState<string>("");
+  const [disabled, setDisabled] = useState<boolean>(false);
+  const [disabledBtnTurn, setDisabledBtnTurn] = useState<boolean>(false);
+  const [mensaje, setMensaje] = useState<string>("");
+  const [radioId, setRadioId] = useState<number>(-1);
+  const [clienteSelected, setClienteSelected] = useState<Cliente | null>(null);
+  const [id, setId] = useState<number>(-1);
+  const [turnos, setTurnos] = useState<Turno[]>([]);
+
+  // ==========================================================================================
+  const { myModule } = useMyModule();
+  // ==========================================================================================
+
+  useEffect(() => {
+    get<{ data: Cliente[] }>("clients").then((res) => setClientes(res.data));
+    get<{ data: Perfil[] }>("attention_profiles").then((res) =>
+      setPerfiles(res.data)
+    );
+    get<{ data: Modulo }>(
+      `modules/ip-address?ip_address=${myModule?.ipAddress}`
+    )
+      .then((res) => {
+        localStorage.setItem("room_id", res.data.room.id.toString());
+      })
+      .then(() => {
+        const sala_id = localStorage.getItem("room_id");
+        if (sala_id)
+          get<{ data: Turno[] }>(
+            `/rooms/${parseInt(sala_id)}/shifts`
+            //`/rooms/${parseInt(sala_id)}/shifts/in-progress`
+            // `/rooms/${parseInt(sala_id)}/shifts/distracted`
+          )
+            .then((res) => {
+              setTurnos(res.data);
+            })
+            .then(() => {
+              get<{ data: Turno[] }>(
+                //`/rooms/${parseInt(sala_id)}/shifts/in-progress`
+                `/rooms/${parseInt(sala_id)}/shifts/distracted`
+              ).then((res) => {
+                setTurnos([...turnos, ...res.data]);
+              });
+            });
+      });
+  }, []);
+
+  useEffect(() => {
+    if (cedula.length < 8 || cedula.length > 10) {
+      setMensaje("La cédula debe tener entre 8 y 10 números");
+      setNombre("");
+      setTipoCliente("");
+      setDisabled(true);
+    } else {
+      const clienteFinded = clientes.find((c) => c.dni == cedula);
+      if (clienteFinded) {
+        setId(clienteFinded.id);
+        setNombre(clienteFinded.name);
+        setTipoCliente(clienteFinded.client_type);
+        setMensaje("Cliente Encontrado");
+        setDisabled(false);
+        setClienteSelected(clienteFinded);
+      } else {
+        setMensaje("Cliente No Encontrado, escriba sus datos");
+        setDisabled(false);
+        setNombre("");
+        setTipoCliente("");
+      }
+    }
+  }, [cedula]);
+
+  useEffect(() => {
+    if (nombre == "" || radioId == -1) setDisabledBtnTurn(true);
+    else setDisabledBtnTurn(false);
+  }, [radioId, nombre]);
+
+  function crearOActualizarCliente() {
+    if (cedula == "" || nombre == "" || tipoCliente == "") {
+      alert("Faltan campos por registrar");
+    } else {
+      if (mensaje == "Cliente Encontrado") editarCliente();
+      else crearCliente();
+    }
+  }
+
+  function cliente2clienteId(tipoCliente: string) {
+    let tipoClienteId: number;
+    if (tipoCliente == "Estandar") tipoClienteId = 3;
+    else if (tipoCliente == "Preferencial") tipoClienteId = 2;
+    else if (tipoCliente == "Tramitador") tipoClienteId = 1;
+    else tipoClienteId = -1;
+    return tipoClienteId;
+  }
+
+  function crearTurno() {
+    if (!clienteSelected || !radioId) {
+      alert("Faltan campos por registrar");
+    } else {
+      const sala_id = localStorage.getItem("room_id");
+      sala_id &&
+        post<{ data: Turno }>("shifts/with-attention-profile", {
+          room_id: parseInt(sala_id),
+          attention_profile_id: radioId,
+          client: {
+            id: clienteSelected.id,
+            name: clienteSelected.name,
+            dni: clienteSelected.dni,
+            is_delete: clienteSelected.is_deleted,
+            client_type_id: cliente2clienteId(clienteSelected.client_type),
+          },
+        }).then((res) => {
+          alert(
+            `Turno de ${res.data.client.name} creado con éxito para el perfil ${res.data.attention_profile}`
+          );
+          setTurnos([res.data, ...turnos]);
+          limpiar();
+        });
+    }
+  }
+
+  function crearCliente() {
+    post<{ data: Cliente }>("clients", {
+      name: nombre,
+      dni: cedula,
+      client_type_id: getClientTypeId(tipoCliente),
+      is_deleted: false,
+    })
+      .then((value) => {
+        // Agregar el nuevo módulo a la lista de módulo registradas
+        setClientes([value.data, ...clientes]);
+        setClienteSelected(value.data);
+      })
+      .then(() => {
+        alert("Cliente guardado");
+        setMensaje("Cliente Encontrado");
+      });
+  }
+
+  function getClientTypeId(name: string): number {
+    if (name == "Estandar") return 3;
+    else if (name == "Preferencial") return 2;
+    else if (name == "Tramitador") return 1;
+    else return -1;
+  }
+
+  function editarCliente() {
+    patch<{ data: Cliente }>(`clients/${id}`, {
+      name: nombre,
+      dni: cedula,
+      client_type_id: getClientTypeId(tipoCliente),
+      is_deleted: false,
+    }).then(() => {
+      const clientinternal = clientes;
+      const nuevosClientes: Cliente[] = [];
+      clientinternal.map((cliente) => {
+        if (cliente.id == id) {
+          const clienteActualizado: Cliente = {
+            id: cliente.id,
+            name: nombre,
+            dni: cedula,
+            client_type: tipoCliente,
+            is_deleted: false,
+          };
+          nuevosClientes.push(clienteActualizado);
+        } else {
+          nuevosClientes.push(cliente);
+        }
+      });
+      setClientes(nuevosClientes);
+      alert("Cliente actualizado");
+    });
+  }
+
+  function eliminar(id: number) {
+    borrar(`shifts/${id}`).then(() => {
+      const turnosActuales = turnos;
+      const nuevosTurnos: Turno[] = [];
+      turnosActuales.map((turno) => {
+        if (turno.id != id) {
+          nuevosTurnos.push(turno);
+        }
+      });
+      setTurnos(nuevosTurnos);
+      alert("Turno eliminado");
+      limpiar();
+    });
+  }
+
+  function limpiar() {
+    setCedula("");
+    setNombre("");
+    setTipoCliente("");
+    setRadioId(-1);
+  }
+
+  return (
+    <div className="container margenbody">
+      <div className="row mt-5">
+        <h2>Reservar Turno</h2>
+        <p className="fs-5">
+          Bienvenido a nuestro sistema de reserva de turnos. Por favor
+          seleccione el tipo de perfil que desea reservar
+        </p>
+      </div>
+      <div className="row">
+        <div className="col-8 cuadroPerfiles">
+          <div className="row d-flex align-items-center justify-content-evenly">
+            <div className="col">
+              <h4>Perfiles disponibles</h4>
+              <p className="fs-6">Seleccione el perfil que desea reservar</p>
+            </div>
+            <div className="col">
+              <div className="form-floating">
+                <input
+                  type="text"
+                  name="buscarPerfil"
+                  id="buscarPerfil"
+                  placeholder="Buscar perfil"
+                  className="buscarPerfil form-control"
+                />
+                <label htmlFor="buscarPerfil" className="fs-6">
+                  Perfil
+                </label>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="row">
+            <div className="col">
+              <ul>
+                {perfiles.map((perfil) => (
+                  <li key={perfil.id} className="listaPerfil w-100">
+                    <input
+                      type="radio"
+                      className="btn-check"
+                      name="perfiles"
+                      id={perfil.id.toString()}
+                      value={perfil.id}
+                      checked={radioId == -1 ? false : true}
+                      onChange={(e) => setRadioId(parseInt(e.target.value))}
+                    />
+                    <label
+                      className={
+                        radioId == perfil.id
+                          ? "btn w-100 perfilSelected"
+                          : "btn w-100"
+                      }
+                      htmlFor={perfil.id.toString()}
+                    >
+                      {perfil.name}
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <button
+              type="button"
+              className="btn btn-primary"
+              disabled={disabledBtnTurn}
+              onClick={crearTurno}
+            >
+              Asignar Turno
+            </button>
+          </div>
+        </div>
+        <div className="col-3 cuadroPerfiles h-50">
+          <div className="row d-flex align-items-center justify-content-evenly">
+            <div className="col">
+              <h4>Buscar cliente</h4>
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  name="buscarCliente"
+                  id="buscarCliente"
+                  placeholder="Buscar Cliente"
+                  className="buscarPerfil form-control"
+                  value={cedula}
+                  onChange={(e) => setCedula(e.target.value)}
+                />
+                <label htmlFor="buscarCliente" className="fs-6">
+                  Cédula
+                </label>
+                <p className="mensaje">{mensaje}</p>
+              </div>
+
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  name="nombreCliente"
+                  id="nombreCliente"
+                  placeholder="Nombre del cliente"
+                  className="buscarPerfil form-control"
+                  value={nombre}
+                  onChange={(e) => setNombre(e.target.value)}
+                  disabled={disabled}
+                />
+                <label htmlFor="nombreCliente" className="fs-6">
+                  Nombre
+                </label>
+              </div>
+
+              <div className="mb-3">
+                <select
+                  name="tipoCliente"
+                  id="tipoCliente"
+                  className="form-select w-100"
+                  // disabled={disabled}
+                  value={tipoCliente}
+                  onChange={(e) => setTipoCliente(e.target.value)}
+                >
+                  <option value="" selected>
+                    Seleccione una opcion
+                  </option>
+                  <option value="Estandar">Estandar</option>
+                  <option value="Preferencial">Preferencial</option>
+                  <option value="Tramitador">Tramitador</option>
+                </select>
+              </div>
+              <div className="mb-3">
+                <button
+                  type="button"
+                  className="btn btn-primary w-100 mb-2"
+                  disabled={disabled}
+                  onClick={crearOActualizarCliente}
+                >
+                  {mensaje == "Cliente Encontrado"
+                    ? "Editar Cliente"
+                    : "Crear Cliente"}
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-warning w-100"
+                  onClick={limpiar}
+                >
+                  Limpiar
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row w-100">
+        <h2>Turnos</h2>
+        <table className="table table-bordered text-center">
+          <thead>
+            <tr>
+              <td>#</td>
+              <td>Nombre usuario</td>
+              <td>Perfil</td>
+              <td>Sala</td>
+              <td>Estado</td>
+              <td>Opciones</td>
+            </tr>
+          </thead>
+          <tbody>
+            {turnos.map((turno, index) => (
+              <tr
+                style={
+                  turno.state == "distracted"
+                    ? { backgroundColor: "#ff000090" }
+                    : { backgroundColor: "#00ff0090" }
+                }
+              >
+                <td>{index + 1}</td>
+                <td>{turno.client.name}</td>
+                <td>{turno.attention_profile}</td>
+                <td>{turno.room}</td>
+                <td>{turno.state}</td>
+                <td>
+                  <button onClick={() => eliminar(turno.id)}>Eliminar</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
