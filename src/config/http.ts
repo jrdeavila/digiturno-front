79d257d3ce7265fbe5cf1abe5e549ecf6a63b7a1
jrdeavila <jrdeavila@unicesar.edu.co>
2024-08-28 08:07:35 -0500

    refactor: Update http.ts to use http instead of https for baseURL

--- src/config/http.ts
+++ src/config/http.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-export const host = "digiturnov2.ccvalledupar.org.co";
-// const host = "localhost";
+const host = "localhost";
 
 const httpClient = axios.create({
-  baseURL: `https://${host}/api`,
+  baseURL: `http://${host}/api`,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
--- src/pusher.js
+++ src/pusher.js
@@ -10,9 +10,9 @@ window.Echo = new Echo({
   // ========================= GENERAL =========================
   broadcaster: 'pusher',
   // ========================= PUSHER =========================
-  key: 'c328570d0ba8235df248',
+  key: 'fa0f8465b73b5d0178ca',
   cluster: 'us2',
-  forceTLS: true,
+  forceTLS: true
   // ========================= ABLY =========================
   // key: "QnWqUQ.ZzBzlQ:ALEpcqSV8npza2IAZFHU5crPcd2MvkBq6MDGownBTsc", // ably
   // wsPort: 443, // ably
